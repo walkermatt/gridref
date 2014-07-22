80a03577f0244a56adcc343d93be8186edcb678b
Matt Walker <walkermatt@longwayaround.org.uk>
2014-07-22 09:10:23 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gridref "0.1.4"
+(defproject gridref "0.1.5"
   :description "Convert an alpha numeric Ordnance Survey grid reference to easting / northing or easting / northing to a grid reference."
   :url "https://github.com/walkermatt/gridref"
   :license {:name "Eclipse Public License"
